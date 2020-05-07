1c306d3c23207a0fb772286d0031138203a18e8a
mflem8 <matthewfleming8@gmail.com>
2020-05-07 14:23:24 -0400

    added JSON variable to API

--- lib/top_breweries/api.rb
+++ lib/top_breweries/api.rb
@@ -1,3 +1,8 @@
 class Api
-
+  def self.get_breweries(city)
+    url = "https://api.openbrewerydb.org/breweries?by_city=#{city}"
+    response = Net::HTTP.get(URI(url))
+    breweries = JSON.parse(response)[0]
+    binding.pry
+  end
 end
--- lib/top_breweries/cli.rb
+++ lib/top_breweries/cli.rb
@@ -3,6 +3,11 @@ class Cli
     puts " "
     puts "Hello and welcome to TopBreweries!"
     puts " "
+    puts "Enter your city to see the breweries in your area:"
+    puts " "
+    @city = gets.strip.downcase
+    Api.get_breweries(@city)
+
   end
 
 end
